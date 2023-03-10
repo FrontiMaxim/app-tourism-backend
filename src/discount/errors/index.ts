d16b2604e5685a97f2adb498177a4f2d5628af23
Максим Расторгуев <frontimax@yandex.ru>
2023-03-10 10:54:20 +0400

    added export via index.ts

---
+++ src/discount/errors/index.ts
@@ -0,0 +1,5 @@
+export * from './discountAlreadyExists.error';
+export * from './discountCreated.error';
+export * from './discountDeleted.error';
+export * from './discountNotFound.error';
+export * from './discountUpdated.error';
